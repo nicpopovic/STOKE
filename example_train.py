736aaaaf306b2c95e576669f4c31e29a6a3bdb7e
nicpopovic <popovic@kit.edu>
2024-11-13 17:45:10 +0100

    update training script

--- example_train.py
+++ example_train.py
@@ -1,16 +1,41 @@
-# imports
+import argparse
 from stoke.src.trainer.util import TrainConfig
 from stoke.src.trainer.trainer import Trainer
 from stoke.src.selection.simple import create_config_for_path
 
-# create TrainConfig object with default values
-config = TrainConfig('data/gpt2/test', n_steps_per_epoch=10, n_epochs=10)
+def parse_args():
+    parser = argparse.ArgumentParser(description="Training configuration")
+    parser.add_argument('--path', type=str, default='data/meta-llama/Llama-3.2-1B-Instruct/STOKE_500_wikiqa', help='Path to the training data')
+    parser.add_argument('--layers', type=int, nargs='+', default=[8, 9, 10, 11, 12], help='List of layers')
+    parser.add_argument('--learning_rates', type=float, nargs='+', default=[1e-4, 5e-5, 3e-4, 5e-4, 1e-3], help='List of learning rates')
+    parser.add_argument('--batch_size', type=int, default=4, help='Batch size')
+    parser.add_argument('--n_epochs', type=int, default=30, help='Number of epochs')
+    parser.add_argument('--n_steps_per_epoch', type=int, default=500, help='Number of steps per epoch')
+    parser.add_argument('--cuda', action='store_true', help='Use CUDA')
+    return parser.parse_args()
 
-# create Trainer
-trainer = Trainer(config)
+def main():
+    args = parse_args()
 
-# run training
-trainer.train()
+    # create TrainConfig object with values from argparse
+    config = TrainConfig(
+        args.path,
+        layers=args.layers,
+        learning_rates=args.learning_rates,
+        batch_size=args.batch_size,
+        n_epochs=args.n_epochs,
+        n_steps_per_epoch=args.n_steps_per_epoch,
+        cuda=args.cuda
+    )
 
-# create basic config for playground
-create_config_for_path(config.path, "basic")
+    # create Trainer
+    trainer = Trainer(config)
+
+    # run training
+    trainer.train()
+
+    # create basic config for playground
+    create_config_for_path(config.path, "basic")
+
+if __name__ == "__main__":
+    main()
